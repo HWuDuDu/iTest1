ac63821d3c9a7e76f784a2512c1c7e499e63ddbc
HWuDuDu <863180248@qq.com>
2018-05-23 16:46:20 +0800

    Initial commit

---
+++ webTest/src/main/java/com/test/Test01.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Test01 {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Test01() {
+    }
+
+
+}
